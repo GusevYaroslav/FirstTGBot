b10604ec82fcd9adb8d8162746cf11d14ef5367b
GusevYaroslav <129690476+GusevYaroslav@users.noreply.github.com>
2023-07-18 15:11:57 +0300

    i

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import random
 from random import randint
 from aiogram import Bot, Dispatcher, executor, types
 
+global i
+i = 0
+
 alf = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -18,11 +21,14 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    i += 1
 
 
 @dp.message_handler(commands=['description'])
 async def send_welcome(message: types.Message):
     await message.reply("Bot's description")
+    i += 1
+
 
 '''
 @dp.message_handler()
@@ -30,6 +36,12 @@ async def echo(message: types.Message):
     symb_num = random.randint(0, len(alf) - 1)
     await message.answer(alf[symb_num])
 '''
+
+
+@dp.message_handler(commands=['count'])
+async def send_welcome(message: types.Message):
+    await message.reply(i)
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
@@ -39,6 +51,7 @@ async def echo(message: types.Message):
     else:
         await message.answer('No')
 
+    i += 1
 
 
 if __name__ == '__main__':
